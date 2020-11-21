011daa59a462f9621d70af3d25268bf4cc6a9776
siroshtan <siroshtan.home@gmail.com>
2020-11-22 01:48:18 +0200

    First commit

---
+++ src/main/java/com/example/intservice/IntServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.intservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/intservice/IntServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.intservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
